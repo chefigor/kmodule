2f523c70a1ef1859a94aa8fa304797f9fc837942
Igor <chef.igor@elfak.rs>
2021-06-20 19:51:44 +0200

    commit

--- main.c
+++ main.c
@@ -7,62 +7,65 @@
 
 #define NETLINK_USER 31
 #define MAX_PAYLOAD 1024
-struct sockaddr_nl src_addr,dest_addr;
-struct nlmsghdr *nlh=NULL;
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
 struct iovec iov;
 int sock_fd;
 struct msghdr msg;
 
 int main()
 {
-    sock_fd=socket(PF_NETLINK,SOCK_RAW,NETLINK_USER);
-    if(sock_fd<0)
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+    if (sock_fd < 0)
         return -1;
 
-    memset(&src_addr,0,sizeof(src_addr));
-    src_addr.nl_family=AF_NETLINK;
-    src_addr.nl_pid=getpid();
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid();
 
-    bind(sock_fd,(struct sockaddr *)&src_addr, sizeof(src_addr));
+    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
 
-    memset(&dest_addr,0,sizeof(dest_addr));
-    dest_addr.nl_family=AF_NETLINK;
-    dest_addr.nl_pid=0;
-    dest_addr.nl_groups=0;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0;
+    dest_addr.nl_groups = 0;
 
-    nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
-    memset(nlh,0,NLMSG_SPACE(MAX_PAYLOAD));
-    nlh->nlmsg_len=NLMSG_SPACE(MAX_PAYLOAD);
-    nlh->nlmsg_pid=getpid();
-    nlh->nlmsg_flags=0;
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
 
-    if(fork()==0){
+    if (fork() == 0)
+    {
         fork();
         fork();
         fork();
-        sleep(20);
+        sleep(200);
     }
-    else{
+    else
+    {
         sleep(5);
-        int pid=getpid();
+        int pid = getpid();
         char mypid[6];
-        sprintf(mypid,"%d",pid);
-        strcpy(NLMSG_DATA(nlh),mypid);
+        sprintf(mypid, "%d", pid);
+        strcpy(NLMSG_DATA(nlh), mypid);
 
-        iov.iov_base=(void*)nlh;
-        iov.iov_len=nlh->nlmsg_len;
-        msg.msg_name=(void *)&dest_addr;
-        msg.msg_namelen=sizeof(dest_addr);
-        msg.msg_iov=&iov;
-        msg.msg_iovlen=1;
+        iov.iov_base = (void *)nlh;
+        iov.iov_len = nlh->nlmsg_len;
+        msg.msg_name = (void *)&dest_addr;
+        msg.msg_namelen = sizeof(dest_addr);
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
 
         printf("Sending message to kernel\n");
-        sendmsg(sock_fd,&msg,0);
+        sendmsg(sock_fd, &msg, 0);
         printf("Waiting for message from kernel\n");
 
-        recvmsg(sock_fd,&msg,0);
-        printf("Recieved message payload:%s\n",NLMSG_DATA(nlh));
+        recvmsg(sock_fd, &msg, 0);
+        printf("Recieved message payload:%s\n", NLMSG_DATA(nlh));
         close(sock_fd);
-    }
 
+        sleep(200);
+    }
 }
